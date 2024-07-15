31e4203fd6bf4b91ce820833b2b40a1129800f2e
KyungsooLee <jmtkd9196@gmail.com>
2024-07-15 10:13:18 +0900

    Init moyeo project

---
+++ moyeo/moyeo/moyeoApp.swift
@@ -0,0 +1,17 @@
+//
+//  moyeoApp.swift
+//  moyeo
+//
+//  Created by kyungsoolee on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct moyeoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
