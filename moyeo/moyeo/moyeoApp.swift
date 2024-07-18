5a6e61108839197863bb27914291fbf098c8d751
Jonghyeon Chang <hokhmah91@naver.com>
2024-07-18 22:40:30 +0900

    feat: 애플 로그인 UI 추가

--- moyeo/moyeo/moyeoApp.swift
+++ moyeo/moyeo/moyeoApp.swift
@@ -6,12 +6,34 @@
 //
 
 import SwiftUI
+import AuthenticationServices
 
 @main
 struct moyeoApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+            TempLoginView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        // 앱이 런칭될 때 호출되는 함수
+        return true
+    }
+    
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity,
+                     restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+        // 앱이 사용자 활동을 계속할 때 호출되는 함수
+        if userActivity.activityType == ASAuthorizationAppleIDProvider.credentialRevokedNotification.rawValue {
+            // Apple ID Credential이 취소된 경우 처리
+            print("Apple ID Credential Revoked")
+        }
+        return true
     }
 }
