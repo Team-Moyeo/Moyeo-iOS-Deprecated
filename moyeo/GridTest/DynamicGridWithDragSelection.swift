914dac8e05c43df1388bf60466e79b76a11daf74
cobalt70 <giwoo.kim@gmail.com>
2024-07-21 10:20:37 +0900

    LazyVgrid Test

---
+++ moyeo/GridTest/DynamicGridWithDragSelection.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  moyeo
+//
+//  Created by Giwoo Kim on 7/20/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
---
+++ moyeo/moyeo/TempGroupVoteView.swift
@@ -0,0 +1,18 @@
+//
+//  TempGroupVoteView.swift
+//  moyeo
+//
+//  Created by Giwoo Kim on 7/18/24.
+//
+
+import SwiftUI
+
+struct TempGroupVoteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TempGroupVoteView()
+}
