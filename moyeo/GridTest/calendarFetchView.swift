2695679b388e029e229d1c1a64f192142b46e301
cobalt70 <giwoo.kim@gmail.com>
2024-07-24 01:46:43 +0900

    Fetching the calendar info  into the timeTable

---
+++ moyeo/GridTest/calendarFetchView.swift
@@ -0,0 +1,18 @@
+//
+//  calendarFetchView.swift
+//  moyeo
+//
+//  Created by Giwoo Kim on 7/22/24.
+//
+
+import SwiftUI
+
+struct calendarFetchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    calendarFetchView()
+}
---
+++ moyeo/calendarViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  calendarViewModel.swift
+//  moyeo
+//
+//  Created by Giwoo Kim on 7/22/24.
+//
+
+import SwiftUI
+
+struct calendarViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    calendarViewModel()
+}
