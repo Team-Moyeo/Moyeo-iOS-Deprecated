61a669b664dc096890e345c943f2559eb7ee8d18
cobalt70 <giwoo.kim@gmail.com>
2024-08-13 13:44:08 +0900

    feat #60 candidatePlace 가져오기

---
+++ moyeo/moyeo/Presentation/View/GroupVote/MapTableView.swift
@@ -0,0 +1,18 @@
+//
+//  MapTableView.swift
+//  moyeo
+//
+//  Created by Giwoo Kim on 8/13/24.
+//
+
+import SwiftUI
+
+struct MapTableView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapTableView()
+}
---
+++ moyeo/moyeo/ViewModel/GroupVote/MapTableViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  MapTableViewModel.swift
+//  moyeo
+//
+//  Created by Giwoo Kim on 8/13/24.
+//
+
+import SwiftUI
+
+struct MapTableViewModel: View {
+    {
+      "timeStamp": "2024-08-13T01:20:02.176Z",
+      "code": "string",
+      "message": "string",
+      "result": {
+        "myVotedPlaces": [
+          "string"
+        ],
+        "totalCandidatePlaces": [
+          {
+            "title": "string",
+            "address": "string",
+            "latitude": 0,
+            "longitude": 0,
+            "voteCount": 0
+          }
+        ],
+        "numberOfPeople": 0
+      }
+    }
+}
+
+#Preview {
+    MapTableViewModel()
+}
